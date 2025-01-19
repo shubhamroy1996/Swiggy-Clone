91d74a73f61ae60c067b54fcc60151319a629838
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2025-01-19 20:02:46 +0530

    add header part

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Swiggy-Clone
-Food delivery application using react.js, where user can order food from any restaurant.
+# Swiggy Clone
+
+Welcome to the Swiggy Clone repository! This project is a full-stack web application built with React.js, Redux Toolkit, Tailwind CSS, and integrates with Swiggy's live APIs to provide a Swiggy-like experience.
+
+## Screenshots
---
+++ src/components/Header/Logo.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <div className="flex items-center space-x-7">
+      <img
+        className="w-12"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_800,h_800/portal/m/logo_192x192.png"
+      />
+      <div className="flex items-center gap-3">
+        <p className="flex hover:text-orange-600 text-ellipsis font-bold underline underline-offset-4">
+          Other
+        </p>
+        <i className="fi text-2xl mt-2 fi-rs-angle-small-down"></i>
+      </div>
+    </div>
+  );
+}
+
+export default Logo;
