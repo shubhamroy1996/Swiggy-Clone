5629f0493c62889e529e3cfe2194033dfb3852a0
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2025-01-22 16:21:28 +0530

    add restaurant menu portion

---
+++ src/components/Body/Restaurant-Menu/DiscountData.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+
+function DiscountData({ discountData }) {
+  const [value, setValue] = useState(0);
+
+  function scrollLeftSide() {
+    value <= 0 ? "" : setValue((prev) => prev - 31);
+  }
+
+  function scrollRightSide() {
+    value >= 124 ? "" : setValue((prev) => prev + 31);
+  }
+
+  return (
+    <div className="w-full overflow-hidden">
+      <div className="flex justify-between mt-8">
+        <h1 className="font-bold mt-4 text-[20px] ml-3">Deals for you</h1>
+
+        <div className="flex gap-3">
+          <div
+            onClick={scrollLeftSide}
+            className={
+              ` cursor-pointer rounded-full w-9 h-9 flex justify-center items-center ` +
+              (value <= 0 ? "bg-gray-100" : "bg-gray-200")
+            }
+          >
+            <i
+              className={
+                `fi text-2xl mt-1 fi-rr-arrow-small-left ` +
+                (value <= 0 ? "text-gray-300" : "text-gray-800")
+              }
+            ></i>
+          </div>
+          <div
+            onClick={scrollRightSide}
+            className={
+              ` cursor-pointer rounded-full w-9 h-9 flex justify-center items-center ` +
+              (value >= 124 ? "bg-gray-100" : "bg-gray-200")
+            }
+          >
+            <i
+              className={
+                `fi text-2xl mt-1 fi-rr-arrow-small-right ` +
+                (value >= 124 ? "text-gray-300" : "text-gray-800")
+              }
+            ></i>
+          </div>
+        </div>
+      </div>
+
+      <div style={{ translate: `-${value}%` }} className=" flex gap-4 mt-5">
+        {discountData.map((data, i) => {
+          return (
+            <div
+              key={i}
+              className="flex gap-2  min-w-[328px] border p-3 h-[76px] rounded-2xl"
+            >
+              <img
+                src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_96,h_96/${data?.info?.offerLogo}`}
+                alt=""
+              />
+
+              <div>
+                <h2 className="flex font-bold text-[18px] capitalize">
+                  {(data?.info?.header).toLowerCase()}
+                </h2>
+                <p className="text-gray-400 flex font-bold text-[14px]">
+                  {data?.info?.couponCode}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default DiscountData;
--- src/components/Body/Restaurant-Menu/RestaurantMenu.jsx
+++ src/components/Body/Restaurant-Menu/RestaurantMenu.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import DiscountData from "./DiscountData";
+import RestaurantInfo from "./RestaurantInfo";
 
 function RestaurantMenu() {
   const [menuData, setMenuData] = useState([]);
@@ -34,93 +36,9 @@ function RestaurantMenu() {
     <>
       <div className="w-full">
         <div className="w-[810px] mx-auto pt-8">
-          <p className="text-[10px] text-slate-500 ">
-            {" "}
-            <Link to={"/"}>
-              {" "}
-              <span className="hover:text-slate-700 hover:cursor-pointer">
-                Home
-              </span>
-            </Link>{" "}
-            /{" "}
-            <Link to={"/"}>
-              {" "}
-              {/* to give extra space between / */}
-              <span className="hover:text-slate-700 hover:cursor-pointer">
-                {restaurantInfo?.locality}
-              </span>
-            </Link>{" "}
-            / <span className="text-slate-700">{restaurantInfo.name}</span>
-          </p>
+          <RestaurantInfo restaurantInfo={restaurantInfo} />
 
-          <h1 className="font-bold pt-6 text-2xl ml-3">
-            {restaurantInfo.name}
-          </h1>
-          <div className="w-full h-[206px] bg-gradient-to-t px-4 pb-4 from-gray-300/70  mt-3 rounded-[30px]">
-            <div className="w-full border  border-slate-200/70 rounded-[30px] h-full bg-white ">
-              <div className="p-4 w-full">
-                <div className="flex items-center gap-1 font-semibold">
-                  <i className="fi fi-ss-circle-star mt-1 text-green-600 text-lg"></i>
-                  <span>{restaurantInfo.avgRating}</span>
-                  <span>({restaurantInfo.totalRatingsString})</span>.
-                  <span>{restaurantInfo.costForTwoMessage}</span>
-                </div>
-
-                <p className="underline font-semibold text-orange-600 ">
-                  {restaurantInfo?.cuisines?.join(", ")}
-                </p>
-
-                <div className="flex gap-2 mt-2">
-                  <div className="w-[9px] flex flex-col justify-center items-center">
-                    <div className="w-[7px] h-[7px] bg-gray-500 rounded-full"></div>
-                    <div className="w-[1px] h-[25px] bg-gray-500 "></div>
-                    <div className="w-[7px] h-[7px] bg-gray-500 rounded-full"></div>
-                  </div>
-                  <div className="flex flex-col gap-1 text-sm font-semibold">
-                    <p>
-                      Outlet{" "}
-                      <span className="text-gray-500 font-normal">
-                        {restaurantInfo.areaName}
-                      </span>
-                    </p>
-                    <p>{restaurantInfo.sla?.slaString}</p>
-                  </div>
-                </div>
-
-                <div className=" w-full">
-                  <div className="flex items-center p-3">
-                    {restaurantInfo.length !== 0 &&
-                    restaurantInfo?.expectationNotifiers ? (
-                      <>
-                        <img
-                          className="w-6"
-                          src={
-                            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_40,h_40/" +
-                            restaurantInfo.feeDetails?.icon
-                          }
-                          alt=""
-                        />
-
-                        <span className="text-sm ml-4 text-gray-500 font-normal">
-                          {restaurantInfo?.expectationNotifiers[0]?.enrichedText?.replace(
-                            /<[^>]*>/g,
-                            ""
-                          )}
-                        </span>
-                      </>
-                    ) : (
-                      ""
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="w-[810px] mx-auto mt-7">
-          <h1 className="font-bold mt-4 text-[20px] ml-3">Deals for you</h1>
-          <div className="flex gap-4 mt-5 border "></div>
+          <DiscountData discountData={discountData} />
         </div>
       </div>
     </>
