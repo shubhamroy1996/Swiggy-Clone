1f6c240dca8adb99d3bde85a0f655c6d4cb50ac0
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2025-01-29 23:32:34 +0530

    add function to search location

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
+import { Visibility, Coordinates } from "./context/contextApi";
 
 import { Route, Routes } from "react-router-dom";
 import RestaurantMenu from "./components/Body/Restaurant-Menu/RestaurantMenu";
 
 function App() {
+  const [visible, setVisible] = useState(false);
+  const [coordinate, setcoordinate] = useState({
+    latitude: 28.7040592,
+    longitude: 77.10249019999999,
+  });
+
   return (
-    <Routes>
-      <Route path="/" element={<Header />}>
-        <Route path="/" element={<Body />} />
-        <Route path="/restaurantMenu/:id" element={<RestaurantMenu />} />
-      </Route>
-    </Routes>
+    <Coordinates.Provider value={{ coordinate, setcoordinate }}>
+      <Visibility.Provider value={{ visible, setVisible }}>
+        <div className={visible ? "max-h-screen overflow-hidden" : ""}>
+          <Routes>
+            <Route path="/" element={<Header />}>
+              <Route path="/" element={<Body />} />
+              <Route path="/restaurantMenu/:id" element={<RestaurantMenu />} />
+            </Route>
+          </Routes>
+        </div>
+      </Visibility.Provider>
+    </Coordinates.Provider>
   );
 }
 
--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Logo() {
+function Logo({ handleVisibility }) {
   return (
-    <Link to={"/"}>
-      <div className="flex items-center space-x-7">
-        <img
-          className="w-12"
-          src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_800,h_800/portal/m/logo_192x192.png"
-        />
-        <div className="flex items-center gap-3">
-          <p className="flex hover:text-orange-600 text-ellipsis font-bold underline underline-offset-4">
-            Other
-          </p>
-          <i className="fi text-2xl mt-2 fi-rs-angle-small-down"></i>
+    <>
+      <Link to={"/"}>
+        <div className="flex items-center space-x-7">
+          <img
+            className="w-12"
+            src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_800,h_800/portal/m/logo_192x192.png"
+          />
+          <div className="flex items-center gap-3" onClick={handleVisibility}>
+            <p className="flex hover:text-orange-600 text-ellipsis font-bold underline underline-offset-4">
+              Other
+            </p>
+            <i className="fi text-2xl mt-2 fi-rs-angle-small-down"></i>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </>
   );
 }
 
