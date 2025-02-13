3c2914e0173cbc995e914951f9c5c2fe0e766b25
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2025-02-13 23:09:52 +0530

    update config file

--- src/config/firebaseAuth.js
+++ src/config/firebaseAuth.js
@@ -2,7 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = JSON.parse(import.meta.env.VITE_KEY)
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
+  };
+
 
 //const app = initializeApp(firebaseConfig);
 
